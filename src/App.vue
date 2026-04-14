ef9a2f9db220df92b15b07746a78146465063c6e
Stuart Grassie <sgrassie@gmail.com>
2026-04-14 07:32:31 +0100

    feat: wire up App.vue, main.ts and router

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+import AppNav from './components/AppNav.vue'
+</script>
+
 <template>
-  <div>Loading…</div>
+  <div class="min-h-screen bg-gray-50">
+    <AppNav />
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
